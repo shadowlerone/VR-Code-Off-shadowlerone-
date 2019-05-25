53f4832be2097b570ac1d953e2ee613e28dc345a
Andrae Lewis <andrae.lewis@gmail.com>
2019-05-25 00:15:53 -0400

    added circumvention warning

--- censor.rb
+++ censor.rb
@@ -2,17 +2,16 @@ def censor_message event, swears_string, circ = false
 	if (event.content =~ /#{swears_string}/)
 		# event.respond "Found Objectionable content"
 		new_string = "**#{event.author.display_name}**:#{event.content.gsub(/#{swears_string}/i, "•"*($&.length))}"
-		#event.message.delete
-		event.channel.send_embed do |embed|
-		embed.add_field(name: nil, value: new_string)
+		event.message.delete
+		event.channel.send_embed do |embed|	
 			if circ == true
 				embed.title = 'Swear Removal Service - Disciplinary Comity'
-				message = "We've noticed you've tried to edit a swear into your message."
-				embed.add_field(name: "Warning", value: message)
+				message = "\n|We've noticed you've tried to edit a swear into your message.\nAttempts to circumvent rules *will* result in a ban.|"
 			else
 				embed.title = 'Swear Removal Service'
 			end
-			embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: 'Language Police', icon_url:"https://cdn.shopify.com/s/files/1/1151/9112/products/image_199487ac-517a-4fbd-a1c4-2853f3de975c_large.png")
+			embed.description = "#{new_string}#{message}"
+			# embed.footer = Discordrb::Webhooks::EmbedFooter.new(text: 'Language Police', icon_url:"https://cdn.shopify.com/s/files/1/1151/9112/products/image_199487ac-517a-4fbd-a1c4-2853f3de975c_large.png")
 		end
 	end
 end
